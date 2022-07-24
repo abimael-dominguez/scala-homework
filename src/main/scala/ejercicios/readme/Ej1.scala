084c1c42f901d9de135a9cb1785dafc29e70f77b
Abimael <abimael.dominguez.perez@gmail.com>
2022-07-23 22:31:20 -0500

    add Ej1 ejercicios.readme

--- src/main/scala/ejercicios/readme/Ej1.scala
+++ src/main/scala/ejercicios/readme/Ej1.scala
@@ -14,16 +14,26 @@ object Ej1 extends App {
     o = R
     u = 4
 
-  Ejercicio: Implementa la lógica correspondiente para que la persona pueda terminar su día laboral.
+    Ejercicio: Implementa la lógica correspondiente para que la persona pueda terminar su día laboral.
 
-  def encryptVowel(vowelsList: List[Char]): List[Char] = ???
+    def encryptVowel(vowelsList: List[Char]): List[Char] = ???
 
-  NOTA: La persona tiene estrictamente prohibido realizar la correspondencia a través de sentencias match, case o if puesto que estaría violando su contrato. No hagas que pierda su empleo :(
+    NOTA: La persona tiene estrictamente prohibido realizar la correspondencia a través de sentencias match,
+    case o if puesto que estaría violando su contrato. No hagas que pierda su empleo :(
 
-  Ejemplo de entrada y salida:
+    Ejemplo de entrada y salida:
 
-  encryptVowel(List('a', 'i', 'e', 'a')) => List(L, $, 9, L) encryptVowel(List('a')) => List(L) encryptVowel(List('i', 'e')) => List($, 9)
+    encryptVowel(List('a', 'i', 'e', 'a')) => List(L, $, 9, L)
+    encryptVowel(List('a')) => List(L)
+    encryptVowel(List('i', 'e')) => List($, 9)
    * */
 
+  def encryptVowel(vowelsList: List[Char]): List[Char] = {
+    val diccionario = Map('a' -> 'L', 'e' -> 9, 'i' -> '$', 'd' -> 4, 'u' -> 4)
+    List(vowelsList.map(diccionario).mkString).flatten
+  }
+  println(encryptVowel(List('a', 'i', 'e', 'a')))
+  println(encryptVowel(List('a')))
+  println(encryptVowel(List('i', 'e')))
 
 }
